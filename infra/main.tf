2b4804e352cd00140ad592dbefad575f27c6d003
AlexanderVasiliev23 <alexandervasiliev23@gmail.com>
2025-05-26 21:29:34 +0300

    Add security group

--- infra/main.tf
+++ infra/main.tf
@@ -81,6 +81,7 @@ resource "yandex_compute_instance" "vm-1" {
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
+    security_group_ids = [yandex_vpc_security_group.vm_sg.id]
   }
 
   metadata = {
